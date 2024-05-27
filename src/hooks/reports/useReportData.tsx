5af90db23590c2ff31f57265da352b06bd5be5bd
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2024-05-27 16:44:25 +0530

    fix : Websocket disconnect issue between routes

--- src/hooks/reports/useReportData.tsx
+++ src/hooks/reports/useReportData.tsx
@@ -1,38 +1,49 @@
 "use client";
 import { CardData, cardData } from "@/lib/data";
 import { useEffect, useState } from "react";
+
 export const useReportsData = () => {
-	const [officeName, setOfficeName] = useState<string | null>(null); 
 	const [reportsData, setReportsData] = useState<CardData[]>(cardData);
 
 	useEffect(() => {
 		if (typeof window !== "undefined") {
-			setOfficeName(localStorage.getItem("officeName"));
+			let socket: WebSocket | null = null;
+
+			const connectWebSocket = () => {
+				try {
+					const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL;
+					const officeName = localStorage.getItem("officeName");
+					if (officeName) {
+						socket = new WebSocket(`${socketUrl}${officeName}`);
+						socket.addEventListener("open", () => {
+							console.log("WebSocket connection established");
+						});
+						socket.addEventListener("message", (event) => {
+							console.log("A MESSAGE IS RECEIVED: ", JSON.parse(event.data));
+							setReportsData((prevData) => [
+								...prevData,
+								JSON.parse(event.data),
+							]);
+						});
+						socket.addEventListener("error", (error) => {
+							console.error("WebSocket error:", error);
+						});
+					}
+				} catch (error) {
+					console.log("Error connecting to WebSocket:", error);
+				}
+			};
+
+			connectWebSocket();
+
+			return () => {
+				if (socket) {
+					socket.close();
+					console.log("WebSocket disconnected");
+				}
+			};
 		}
-		const connectWebSocket = () => {
-			try {
-				const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL;
-				const socket = new WebSocket(`${socketUrl}${officeName}`);
-
-				socket.addEventListener("open", () => {
-					console.log("WebSocket connection established");
-				});
-
-				socket.addEventListener("message", (event) => {
-					console.log("A MESSAGE IS RECEIVED: ", JSON.parse(event.data));
-					setReportsData((prevData) => [...prevData, JSON.parse(event.data)]);
-				});
-
-				socket.addEventListener("error", (error) => {
-					console.error("WebSocket error:", error);
-				});
-			} catch (error) {
-				// console.log("Error connecting to WebSocket:", error);
-			}
-		};
-
-		connectWebSocket();
-	}, [officeName]);
+	}, []);
 
 	return { reportsData };
 };
